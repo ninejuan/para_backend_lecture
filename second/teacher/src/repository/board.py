8bfa27762dc48aec6e3087f0152562d9d860b215
Juan Lee <me@juany.kr>
2025-04-29 11:42:26 +0900

    add: second lecture board model

---
+++ second/teacher/src/repository/board.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, Field
+import uuid
+import datetime as dt
+
+class Article(BaseModel):
+    id: str = Field(default_factory=lambda: str(uuid.uuid4()), description="게시글의 고유 식별자입니다. 자동으로 생성됩니다.")
+    title: str = Field(..., description="게시글의 제목입니다.", examples=["[25] PARA ANN 수업자료"])
+    content: str = Field(..., description="게시글의 내용입니다.", examples=["PARA 인공신경망 수업자료입니다."])
+    createdAt: dt.date = Field(..., default=dt.datetime.now())
+
