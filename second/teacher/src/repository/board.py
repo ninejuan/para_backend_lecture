66bdb49aeb58edf358b585619d8603f9d4fc3c38
Juan Lee <me@juany.kr>
2025-04-29 12:07:45 +0900

    add: second lecture student code

--- second/teacher/src/repository/board.py
+++ second/teacher/src/repository/board.py
@@ -6,5 +6,5 @@ class Article(BaseModel):
     id: str = Field(default_factory=lambda: str(uuid.uuid4()), description="게시글의 고유 식별자입니다. 자동으로 생성됩니다.")
     title: str = Field(..., description="게시글의 제목입니다.", examples=["[25] PARA ANN 수업자료"])
     content: str = Field(..., description="게시글의 내용입니다.", examples=["PARA 인공신경망 수업자료입니다."])
-    createdAt: dt.date = Field(..., default=dt.datetime.now())
+    createdAt: dt.datetime = Field(default_factory=dt.datetime.now, description="게시글이 생성된 시간입니다.")
 
